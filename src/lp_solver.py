d9fb7ab9eb949e5978ecad2a804f4d7b01515b31
Ali <ali2moh.04@gmail.com>
2026-04-10 22:14:01 +0300

    Implemented LP solver using pulp

--- src/lp_solver.py
+++ src/lp_solver.py
@@ -1,2 +1,39 @@
+import pulp as p
+
 def solve_vertex_cover_lp(G):
-    pass
+    """
+    Solve the LP relaxation of the Minimum Vertex Cover problem.
+
+    Parameters:
+        G (networkx.Graph): input graph
+
+    Returns:
+        dict: {
+            "status": str,
+            "objective": float,
+            "x_values": dict
+        }
+    """
+    Lp_prob = p.LpProblem('Vertex_Cover_LP', p.LpMinimize) 
+
+    x = {
+        v: p.LpVariable(f"x_{v}", lowBound=0, upBound=1, cat="Continuous")
+        for v in G.nodes()
+    }
+
+    Lp_prob += p.lpSum(x[v] for v in G.nodes()), "Minimize_Vertex_Cover_Size"
+
+    for u, v in G.edges():
+        Lp_prob += x[u] + x[v] >= 1, f"cover_edge_{u}_{v}"
+
+    Lp_prob.solve(p.PULP_CBC_CMD(msg = False))
+    
+    status = p.LpStatus[Lp_prob.status]
+    objective = p.value(Lp_prob.objective)
+    x_values = {v: p.value(x[v]) for v in G.nodes()}
+
+    return {
+        "status": status,
+        "objective": objective,
+        "x_values": x_values,
+    }
