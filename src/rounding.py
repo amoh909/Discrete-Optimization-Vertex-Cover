b1b4f6ca9714bcd9bd47f193a29f5c04aabd6dd8
Ali <ali2moh.04@gmail.com>
2026-04-11 21:18:02 +0300

    Validated Rounding Algorithm

--- src/rounding.py
+++ src/rounding.py
@@ -3,14 +3,17 @@ def threshold_rounding(x_values, threshold=0.5):
     Converts a fractional LP solution into an integral vertex cover using threshold rounding.
     
     Parameters:
-    x_values (dict): dictionary mapping each vertex to its LP value
-    threshold (float): vertices with value >= threshold are included in the vertex cover
+        x_values (dict): dictionary mapping each vertex to its LP value
+        threshold (float): rounding threshold (default = 0.5 for this problem)
     Returns:
-        list: vertices selected after rounding
+        list: vertices selected after rounding (vertex cover)
     """
-    rounded_cover = [] ## stores all the vertices selected after rounding 
-    for v, value in x_values.items():
-        if value >= threshold: ## Add the vertex to the cover if its LP value meets the threshold
-            rounded_cover.append(v) 
+    epsilon = 1e-9
+    threshold = threshold - epsilon ## to avoid floating-point precision errors
+    
+    rounded_cover = {
+    node for node, value in x_values.items() 
+        if value >= (threshold)
+    }
              
     return rounded_cover
